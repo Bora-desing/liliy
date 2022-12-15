2d30b5141348f49bc59d7ce42f9ca13f81ea7539
Bora Yiğit Elik <77041562+Bora-desing@users.noreply.github.com>
2022-12-15 19:48:58 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Nostge-Ekibi
+
+👑- Bora Yiğit  
+👔- Batu  
+👔- Berkan  
+👔- Alcovra
+
+# Linkler
+<a href="https://youtube.com/NOSTGE"><img src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white"></a>
+<a href="https://discord.gg/AgSMqvPr8U"><img src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white"></a>
---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    token: "MTAyNTg1ODk3NDkzNzAwMjA3Ng.Gr_722.53FkuHVtZL0VoVGNK_zDy9lMsXyMIyORjyidic", 
+    prefix: "+",
+    botdavet: "https://discord.com/api/oauth2/authorize?client_id=1025858974937002076&permissions=8&scope=bot%20applications.commands",
+    desteksunucusu: "https://discord.gg/GmVKUh3Mt9",
+    website: "https://liliy.tk/",
+    topgg: "https://top.gg/bot/1025858974937002076",
+    politika: "(zorunlu)",
+    sunucuid: "1025855860427202741",
+    destekçi: "1040690692839718942",
+    instagram:"https://www.instagram.com/nostge_tr/"
+}
+
